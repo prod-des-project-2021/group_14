887e344397a0e64e1d78ddea3f321820a340777a
Joona Suhonen <59951268+JoonaSuhonen@users.noreply.github.com>
2021-11-24 19:18:57 +0200

    2nd
    Toimiva kameraohjaus pelaajalle

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,32 +4,57 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    public float RotationSpeed = 1;
-    public Transform Target, Player;
-    float mouseX, mouseY;
+    public float rotateSpeed;
+    public Transform player;
+    public Vector3 offset;
+    public bool useOffsetValues;
+    public Transform pivot;
 
-    void Start()
+    void Start() 
     {
-        Cursor.visible = false;
+    if (!useOffsetValues)
+        {
+            offset = player.position - transform.position;
+        }
+    
+        pivot.transform.position = player.transform.position;
+        pivot.transform.parent = player.transform;
+
         Cursor.lockState = CursorLockMode.Locked;
     }
 
+   
+
     void LateUpdate()
     {
-        CamControl();
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        player.Rotate(0, horizontal,0);
+
+        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+        pivot.Rotate(vertical, 0, 0);
+
+        if(pivot.rotation.eulerAngles.x > 45f && pivot.rotation.eulerAngles.x < 180f)
+        {
+            pivot.rotation = Quaternion.Euler(45f, 0, 0);
+        }
+
+        if(pivot.rotation.eulerAngles.x > 180 && pivot.rotation.eulerAngles.x < 315f)
+        {
+            pivot.rotation = Quaternion.Euler(315f, 0, 0);
+        }
+        
+
+
+        float desiredYAngle = player.eulerAngles.y;
+        float desiredXAngle = pivot.eulerAngles.x;
+        Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
+        transform.position = player.position - (rotation * offset);
+
+        if(transform.position.y < player.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, player.position.y, transform.position.z);
+        }
+        transform.LookAt(player);
+
     }
-
-    void CamControl()
-    {
-        mouseX += Input.GetAxis("Mouse X") * RotationSpeed;
-        mouseY -= Input.GetAxis("Mouse Y") * RotationSpeed;
-        mouseY = Mathf.Clamp(mouseY, -35, 60);
-
-        transform.LookAt(Target);
-
-        Target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
-        Player.rotation = Quaternion.Euler(0, mouseX, 0);
-
-    }
-
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody rb;
+    public Transform cameraTarget;
+    public Transform MainCamera;
+    Vector2 input;
+    float heading;
+    float rotation;
     [SerializeField] float movementSpeed = 5f;
     [SerializeField] float jumpForce = 5f;
     [SerializeField] Transform groundCheck;
@@ -18,11 +23,31 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-
-        rb.velocity = new Vector3(horizontalInput * movementSpeed, rb.velocity.y, verticalInput * movementSpeed);
         
+        //float horizontalInput = Input.GetAxis("Horizontal");
+        //float verticalInput = Input.GetAxis("Vertical");
+        rotation += Input.GetAxis("Mouse Y")*Time.deltaTime*180;
+        heading += Input.GetAxis("Mouse X")*Time.deltaTime*180;
+        cameraTarget.rotation = Quaternion.Euler(0, heading, 0);
+        cameraTarget.rotation = Quaternion.Euler(-rotation, heading, 0);
+
+        //rb.velocity = new Vector3(horizontalInput * movementSpeed, rb.velocity.y, verticalInput * movementSpeed);
+        input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        input = Vector2.ClampMagnitude(input, 1);
+        
+        Vector3 camF = MainCamera.forward;
+        Vector3 camR = MainCamera.right;
+
+        camF.y = 0;
+        camR.y = 0;
+        camF = camF.normalized;
+        camR = camR.normalized;
+
+        transform.position += (camF*input.y + camR*input.x)*Time.deltaTime*movementSpeed;
+        //transform.position += new Vector3(input.x,0,input.y)*deltaTime*5;
+        
+
+
         if (Input.GetButtonDown("Jump") && isGrounded())
         {
             Jump();
@@ -41,6 +66,12 @@ public class PlayerMovement : MonoBehaviour
             Destroy(collision.transform.parent.gameObject);
             Jump();
         }
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            rb = collision.gameObject.GetComponent<Rigidbody>();
+            rb.angularVelocity = Vector3.zero;
+        }
+        
     }
 
     bool isGrounded()
