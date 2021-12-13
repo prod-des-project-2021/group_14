97087420d243e7cb9a6317c65302a5f399ea7bb3
t9niro00 <t9niro00@students.oamk.fi>
2021-12-13 22:07:18 +0200

    Testausta tuleville ominaisuuksille

---
+++ Assets/Scripts/Menu_Buttons.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu_Buttons : MonoBehaviour
+{
+    public GameObject MenuPanel;
+    public GameObject LevelSelectPanel;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MenuPanel.SetActive(false);
+        LevelSelectPanel.SetActive(false);
+    }
+
+    public void ShowLevelPanel()
+    {
+        MenuPanel.SetActive(false);
+        LevelSelectPanel.SetActive(true);
+    }
+
+
+    public void ShowMenuPanel()
+    {
+        MenuPanel.SetActive(true);
+        LevelSelectPanel.SetActive(false);
+    }
+
+    void Update()
+    {
+    if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            MenuPanel.SetActive(!MenuPanel.gameObject.activeSelf);
+            LevelSelectPanel.SetActive(false);
+        }
+    }
+}
